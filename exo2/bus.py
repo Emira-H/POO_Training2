d011b465c59ea4ec8ada2e178f783537d5683b7a
Emira_Harrioui <harrioui.e@gmail.com>
2019-12-13 16:43:47 +0100

    Exercice 2 finished

--- exo2/bus.py
+++ exo2/bus.py
@@ -2,7 +2,18 @@ from vehicule import Vehicule
 '''class daughter from class vehicule'''
 
 class Bus(Vehicule):
-    number_floors = (1,2)
+    floors = (1,2)
     def __init__(self, immatriculation, color, number_floors):
         super().__init__(immatriculation, color)
         self.number_floors = number_floors
+
+    @property
+    def number_floors(self):
+        return self.__number_floors
+
+    @number_floors.setter
+    def number_floors(self, number_floors):
+        if number_floors in Bus.floors:
+            self.__number_floors= number_floors
+        else:
+            raise ValueError("this number of floors is not available!")
--- exo2/voiture.py
+++ exo2/voiture.py
@@ -3,7 +3,18 @@ from vehicule import Vehicule
 '''class daughter from class vehicule'''
 
 class Voiture(Vehicule):
-    number_doors = (3, 5)
+    doors = (3, 5)
     def __init__(self, immatriculation, color, number_doors):
         Vehicule.__init__(self, immatriculation, color)
         self.number_doors = number_doors
+
+    @property
+    def number_doors(self):
+        return self.__number_doors
+
+    @number_doors.setter
+    def number_doors(self, number_doors):
+        if number_doors in Voiture.doors:
+            self.__number_doors = number_doors
+        else:
+            raise ValueError("this number of doors is not available!")
